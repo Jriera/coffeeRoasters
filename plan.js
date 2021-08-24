9ed4c51d3a8d18e302014e75b7347bfa27b5a2b1
Jriera <jordiriera81@gmail.com>
2021-08-24 14:13:57 +0200

    onclick assignment

--- plan.js
+++ plan.js
@@ -164,4 +164,4 @@ var monthlySelected = function(){
     document.getElementById("delivery-selection").innerHTML="monthly";
 }
 
-capsule.onclick(capsuleSelected());
+capsule.onclick=capsuleSelected();
